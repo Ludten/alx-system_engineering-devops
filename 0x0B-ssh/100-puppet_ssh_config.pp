e5b73b93a2fa434be9969312271c4b46ba5de7be
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-18 20:10:33 +0100

    ssh
    task 4; automated file editing.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# edit ssh config file
+file_line { 'identityfile'
+  ensure  =>  present,
+  path    =>  '/etc/ssh/ssh_config',
+  line    =>  'IdentityFile ~/.ssh/school',
+  match   =>  '^IdentityFile'
+}
+
+file_line { 'passwd_auth'
+  ensure  =>  present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   =>  '^PasswordAuthentication'
+}
