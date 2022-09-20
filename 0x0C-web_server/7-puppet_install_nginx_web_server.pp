3aec225b6e71ffbd18197a3ec01891aa00b1e87e
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-20 13:42:10 +0100

    web server
    puppet linting double quoted string

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,7 +10,7 @@ service { 'nginx':
 file { 'index':
   ensure  =>  file,
   path    =>  '/var/www/html/index.html',
-  content =>  "Hello World!"
+  content =>  'Hello World!'
 }
 file_line { 'redirect':
   ensure  =>  present,
