ccc1106c67a9a71f244f5beba4d9f395a319348b
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-20 13:54:04 +0100

    web server
    puppet config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # configure an nginx web server
 package { 'nginx':
-  ensure  =>  latest
+  ensure  =>  installed
 }
 file { 'index':
   ensure  =>  file,
@@ -8,13 +8,12 @@ file { 'index':
   content =>  "Hello World!\n"
 }
 file_line { 'redirect':
-  ensure  =>  present,
-  path    =>  '/etc/nginx/sites-available/default',
-  line    =>  "\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=dQw4w9WgXcQ\\ permanent;",
-  after   =>  'server_name _;'
+  ensure =>  present,
+  path   =>  '/etc/nginx/sites-available/default',
+  line   =>  "\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=dQw4w9WgXcQ\\ permanent;",
+  after  =>  'server_name _;'
 }
 service { 'nginx':
   ensure  =>  running,
-  enable  =>  true,
   require =>  Package['nginx']
 }
