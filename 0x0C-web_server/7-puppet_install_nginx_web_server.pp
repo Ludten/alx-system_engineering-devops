e5a3d7262559ea40e7843b1b02ee77a57def9b60
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-20 13:39:00 +0100

    web server
    added service notifier

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,11 +10,13 @@ service { 'nginx':
 file { 'index':
   ensure  =>  file,
   path    =>  '/var/www/html/index.html',
-  content =>  "Hello World!\n"
+  content =>  "Hello World!"
 }
 file_line { 'redirect':
   ensure =>  present,
+  notify  =>  Service['nginx'],
   path   =>  '/etc/nginx/sites-available/default',
+  require =>  Package['nginx'],
   line   =>  "\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=dQw4w9WgXcQ\\ permanent;",
   after  =>  'server_name _;'
 }
