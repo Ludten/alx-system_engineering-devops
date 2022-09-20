691e20e1920fba8bdf6f1bfbd5e724a1ba4b94e5
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-20 13:52:06 +0100

    web server
    started service after configuration.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,6 @@
 # configure an nginx web server
 package { 'nginx':
-  ensure  =>  installed
-}
-service { 'nginx':
-  ensure  =>  running,
-  enable  =>  true,
-  require =>  Package['nginx']
+  ensure  =>  latest
 }
 file { 'index':
   ensure  =>  file,
@@ -14,9 +9,12 @@ file { 'index':
 }
 file_line { 'redirect':
   ensure  =>  present,
-  notify  =>  Service['nginx'],
   path    =>  '/etc/nginx/sites-available/default',
-  require =>  Package['nginx'],
   line    =>  "\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=dQw4w9WgXcQ\\ permanent;",
   after   =>  'server_name _;'
+}
+service { 'nginx':
+  ensure  =>  running,
+  enable  =>  true,
+  require =>  Package['nginx']
 }
