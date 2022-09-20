a83cb62fed2b2b6b2d96febf2c4216c2fbdeefe8
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-20 13:37:59 +0100

    web server
    reading is hard for ygo players

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,27 +10,11 @@ service { 'nginx':
 file { 'index':
   ensure  =>  file,
   path    =>  '/var/www/html/index.html',
-  content =>  "Hello World!"
+  content =>  "Hello World!\n"
 }
 file_line { 'redirect':
   ensure =>  present,
   path   =>  '/etc/nginx/sites-available/default',
   line   =>  "\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=dQw4w9WgXcQ\\ permanent;",
   after  =>  'server_name _;'
-}
-file { 'custom_404':
-  ensure  =>  file,
-  path    =>  '/usr/share/nginx/html/custom_404.html',
-  content =>  "Ceci n'est pas une page"
-}
-
-$new_line = "\n\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}"
-
-file_line { 'error_404':
-  ensure  =>  present,
-  notify  =>  Service['nginx'],
-  path    =>  '/etc/nginx/sites-available/default',
-  require =>  Package['nginx'],
-  line    =>  $new_line,
-  after   =>  'rewrite \^\/redirect_me\/\$ https\:\/\/www\.youtube\.com\/watch\?v=dQw4w9WgXcQ\\ permanent;'
 }
