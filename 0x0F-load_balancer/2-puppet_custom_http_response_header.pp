53ab21048076cf654eb0d0836e4afff489f7654c
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-26 13:31:59 +0100

    Load Balancer
    changed positon

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,19 +8,9 @@ package { 'nginx':
   ensure  =>  installed,
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-file_line { 'index':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'server_name _;',
-  line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
-}
-
 file_line { 'add_headr':
   path  => '/etc/nginx/sites-available/default',
-  after => '^[ \t]{1,}location \/ {',
+  after => 'server_name _;',
   line  => 'add_header X-Served-By $hostname always;',
 }
 
