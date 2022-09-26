d1a54f8e6dd44cc32b253e12c4622c6074126cc0
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-26 13:05:06 +0100

    Load Balancer
    Puppet script for custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# configure an nginx web server
+exec { 'Update apt repository':
+  command => 'apt-get -y update',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+
+package { 'nginx':
+  ensure  =>  installed,
+}
+
+file_line { 'add_headr':
+  path  => '/etc/nginx/sites-available/default',
+  after => '^[ \t]{1,}location \/ {',
+  line  => 'add_header X-Served-By $hostname always;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
