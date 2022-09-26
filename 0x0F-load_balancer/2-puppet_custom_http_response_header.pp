30757ec6d5f0ad67f4e07e1343b3d306016485c0
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-26 13:36:16 +0100

    Load Balancer
    added require

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,10 +8,25 @@ package { 'nginx':
   ensure  =>  installed,
 }
 
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file_line { 'index':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
+  require => Package['nginx'],
+}
+
 file_line { 'add_headr':
+  ensure => present,
   path  => '/etc/nginx/sites-available/default',
-  after => 'server_name _;',
+  after => '^[ \t]{1,}location \/ {',
   line  => 'add_header X-Served-By $hostname always;',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
