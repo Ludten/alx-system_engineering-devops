1278a4fb53983ebbe3eb8025c0db423246abeee9
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-26 13:22:17 +0100

    Load Balancer
    Added puppet commands from older script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,15 +8,23 @@ package { 'nginx':
   ensure  =>  installed,
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'index':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
 }
 
 file_line { 'add_headr':
-  notify  => Service['nginx'],
   path    => '/etc/nginx/sites-available/default',
   after   => '^[ \t]{1,}location \/ {',
   line    => 'add_header X-Served-By $hostname always;',
+}
+
+service { 'nginx':
+  ensure  => running,
   require => Package['nginx'],
 }
