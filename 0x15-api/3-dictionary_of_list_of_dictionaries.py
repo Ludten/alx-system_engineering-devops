946293a7b0e5253fc19d980fda999af590d13065
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-10-17 20:04:02 +0100

    API
    All users of the jsonplaceholder api

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A module that gathers data from an API and exports to JSON
+format
+"""
+
+import json
+
+
+def save_to_file(obj):
+    """
+    converts todo instances to their json representation
+    and saves them as text files
+    """
+    j_obj = json.dumps(obj)
+    with open("todo_all_employees.json", 'w',
+              encoding="utf-8") as f:
+        f.write(j_obj)
+
+
+if __name__ == "__main__":
+    import requests
+
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users')
+    try:
+        body = name.json()
+        jlist = []
+        jsdict = {}
+        for item in body:
+            user_id = item['id']
+            r = requests.get(
+                'https://jsonplaceholder.typicode.com/users/{:d}/todos'
+                .format(user_id)).json()
+            for task in r:
+                jdict = {}
+                jdict['username'] = item['username']
+                jdict['task'] = task['title']
+                jdict['completed'] = task['completed']
+                jlist.append(jdict)
+            jsdict['{}'.format(user_id)] = jlist
+        save_to_file(jsdict)
+    except Exception:
+        pass
