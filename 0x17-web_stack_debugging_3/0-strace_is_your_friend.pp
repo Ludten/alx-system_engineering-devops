67c30deebeb19bab5c0212253b278b87c0bc85a8
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-11-03 03:53:27 +0100

    Web Stack Debugging
    Added path to command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Fix apache wordpress server
 exec { 'fix_apache_wordpress':
-  command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  command => ''/usr/bin/env sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
 }
