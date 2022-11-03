3221a962b69f25716398324b195ccb591dd40f6a
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-11-03 03:43:28 +0100

    Web Stack Debugging
    corrected file name inside settings

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Fix apache wordpress server
 exec { 'fix_apache_wordpress':
-  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
 }
